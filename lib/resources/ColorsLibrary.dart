5c61ba004c87b89086dcb3704a8eaec98a35521f
Fliolli <maryolford@gmail.com>
2021-03-24 01:20:57 +0300

    Add ColorsLibrary and customize bottom navbar

---
+++ lib/resources/ColorsLibrary.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsLibrary {
+  static final Color neutralGray = Color(0xFFD7CCC8);
+  static final Color lightGray = Color(0xFFEFEBE9);
+  static final Color primaryColor = Color(0xFFF06292);
+  static final Color whiteColor = Color(0xFFFFFFFF);
+}
